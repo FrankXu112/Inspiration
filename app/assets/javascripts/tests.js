aa4338fda51bc544516a3a758462b1c20cb4f3b1
Patricia Kore <patricia.kore@yahoo.com>
2015-06-13 19:09:30 -0400

    tests controller and new view

---
+++ app/assets/javascripts/tests.js
@@ -0,0 +1,11 @@
+// $(document).on('ready page:load', function() {
+
+// $('#yes_or_no').change(function(){
+
+//   // var myvalue=$('#yes_or_no option:selected').value()
+//   // if (myvalue=="No"){$('#major').hide()}
+//   alert("hello");
+
+// });
+
+// });
