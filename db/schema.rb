30d95656e913f8dec044958bb287f035b5848c3d
Sabina <sabina_stoicescu@yahoo.com>
2015-06-13 22:38:33 -0400

    deleted yield

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150613200554) do
+
+  create_table "stories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "experience"
+    t.string   "job"
+    t.string   "grades"
+    t.string   "first_choice"
+    t.string   "second_choice"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "study"
+    t.string   "dislike"
+    t.string   "hobby"
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -34,23 +48,4 @@ ActiveRecord::Schema.define(version: 20150613200554) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
-
-ActiveRecord::Schema.define(version: 20150613185146) do
-
-  create_table "stories", force: :cascade do |t|
-    t.string   "name"
-    t.string   "experience"
-    t.string   "job"
-    t.string   "grades"
-    t.string   "first_choice"
-    t.string   "second_choice"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.string   "study"
-    t.string   "dislike"
-    t.string   "hobby"
-    t.integer  "user_id"
-  end
-
->>>>>>> sabina
 end
